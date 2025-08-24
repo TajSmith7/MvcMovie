0f080c66a2022e8ee45889ba1de9972d195b94f9
tajsm <tajsm@THE_HUB>
2025-08-24 17:47:21 -0400

    Add project files.

---
+++ MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using MvcMovie.Models;
+using MvcMovie.Helpers;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MvcMovieContext _context;
+
+        private readonly ILogger<MoviesController> _logger;
+
+        public MoviesController(MvcMovieContext context, ILogger<MoviesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
+        {
+            if (_context.Movie == null)
+            {
+                _logger.Error("Movie is null.");
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+                _logger.Debug("User searched for movies with letters: {searchString}", searchString);
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+                _logger.Debug("User filtered movies by genre: {movieGenre}", movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
+        }
+
+        [HttpPost]
+        public string Index(string searchString, bool notUsed)
+        {
+            return "From [HttpPost]Index: filter on " + searchString;
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                _logger.Warn("Movie ID is null.");
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie == null)
+            {
+                _logger.Warn("Movie with ID {id} not found.", id);
+                return NotFound();
+            }
+
+            _logger.Info("Viewing details for movie with ID {id}.", id);
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            _logger.Info("Accessed the Create Movie page.");
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _logger.Warn("Creating a new movie with title: {Title}", movie.Title);
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                _logger.Warn("Movie ID is null for edit operation.");
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie == null)
+            {
+                _logger.Warn("Movie with ID {id} not found for edit operation.", id);
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                _logger.Warn("Movie ID mismatch for edit operation. Provided ID: {id}, Movie ID: {movieId}", id, movie.Id);
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _logger.Warn("Editing movie with ID: {id}", id);
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        _logger.Warn("Movie with ID {id} no longer exists during edit operation.", movie.Id);
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                _logger.Warn("Movie ID is null for delete operation.");
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                _logger.Warn("Movie with ID {id} not found for delete operation.", id);
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie != null)
+            {
+                _logger.Warn("Deleting movie with ID: {id}", id);
+                _context.Movie.Remove(movie);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movie.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "The Lion King",
+                    ReleaseDate = DateTime.Parse("1994-6-15"),
+                    Genre = "Family Musical",
+                    Price = 10.00M,
+                    Rating = "G"
+                },
+                new Movie
+                {
+                    Title = "Harry Potter and the Sorcerer's Stone",
+                    ReleaseDate = DateTime.Parse("2001-11-16"),
+                    Genre = "Family Fantasy",
+                    Price = 8.08M,
+                    Rating = "PG"
+                },
+                new Movie
+                {
+                    Title = "Raya and the Last Dragon",
+                    ReleaseDate = DateTime.Parse("2021-3-5"),
+                    Genre = "Family Fantasy",
+                    Price = 13.89M,
+                    Rating = "PG"
+                },
+                 new Movie
+                 {
+                     Title = "Charlie and the Chocolate Factory",
+                     ReleaseDate = DateTime.Parse("2005-7-10"),
+                     Genre = "Family Fantasy",
+                     Price = 5.99M,
+                     Rating = "PG"
+                 },
+                new Movie
+                {
+                    Title = "Rio",
+                    ReleaseDate = DateTime.Parse("2011-4-15"),
+                    Genre = "Family Adventure",
+                    Price = 8.99M,
+                    Rating = "G"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
